87ba9ce562f1699ae755bda71c46a99b314309b1
abraham <rulanfenghua@163.com>
2018-08-07 00:04:16 +0800

    Star

---
+++ src/components/Star.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="star" :class="starType">
+    <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item" :key="itemClass.index"></span>
+  </div>
+</template>
+
+<script>
+const LENGTH = 5;
+
+export default {
+  name: 'star',
+  props: {
+    size: {
+      type: Number
+    },
+    score: {
+      type: Number
+    }
+  },
+  computed: {
+    starType() {
+      return 'star-' + this.size;
+    },
+    itemClasses() {
+      let result = [];
+      let score = Math.floor(this.score * 2) / 2;
+      let hasDecimal = score % 1 !== 0;
+      let integer = Math.floor(score);
+
+      for (let i = 0; i < integer; i++) {
+        result.push('on');
+      }
+      if (hasDecimal) {
+        result.push('half');
+      }
+      while (result.length < LENGTH) {
+        result.push('off');
+      }
+      return result;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  @import '@/design/index.scss';
+
+  .star {
+    .star-item {
+      display: inline-block;
+      background-repeat: no-repeat;
+    }
+    &.star-48 {
+      .star-item {
+        width: 20px;
+        height: 20px;
+        margin-right: 22px;
+        background-size: 100%;
+        &:last-child{
+          margin-right: 0;
+        }
+        &.on{
+          @include bg-image('Star/star48_on');
+        }
+        &.half{
+          @include bg-image('Star/star48_half');
+        }
+        &.off{
+          @include bg-image('Star/star48_off');
+        }
+      }
+    }
+    &.star-36 {
+      .star-item {
+        width: 15px;
+        height: 15px;
+        margin-right: 6px;
+        background-size: 100%;
+        &:last-child{
+          margin-right: 0;
+        }
+        &.on{
+          @include bg-image('Star/star36_on');
+        }
+        &.half{
+          @include bg-image('Star/star36_half');
+        }
+        &.off{
+          @include bg-image('Star/star36_off');
+        }
+      }
+    }
+    &.star-24 {
+      .star-item {
+        width: 10px;
+        height: 10px;
+        margin-right: 3px;
+        background-size: 100%;
+        &:last-child{
+          margin-right: 0;
+        }
+        &.on{
+          @include bg-image('Star/star24_on');
+        }
+        &.half{
+          @include bg-image('Star/star24_half');
+        }
+        &.off{
+          @include bg-image('Star/star24_off');
+        }
+      }
+    }
+  }
+</style>
--- src/views/VHeader.vue
+++ src/views/VHeader.vue
@@ -15,12 +15,12 @@
             <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div class="supports-count" v-if="seller.supports">
+      <div class="supports-count" v-if="seller.supports" @click="showDetail">
         <span class="count">{{seller.supports.length+'个'}}</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="title"></span>
       <span class="text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
@@ -28,15 +28,41 @@
     <div class="background">
       <img :src="seller.avatar" alt="" width="100%" height="100%">
     </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <star :size="48" :score="seller.score"></star>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import star from '@/components/Star.vue';
+
 export default {
   name: 'v-header',
+  components: {
+    star
+  },
   props: {
     seller: {
       type: Object
+    }
+  },
+  data() {
+    return {
+      detailShow: false
+    };
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true;
     }
   },
   created() {
@@ -102,19 +128,19 @@ export default {
             margin-right: 4px;
             background-size: 12px;
             background-repeat: no-repeat;
-            &.decrease{
+            &.decrease {
               @include bg-image('VHeader/decrease_1');
             }
-            &.discount{
+            &.discount {
               @include bg-image('VHeader/discount_1');
             }
-            &.guarantee{
+            &.guarantee {
               @include bg-image('VHeader/guarantee_1');
             }
-            &.invoice{
+            &.invoice {
               @include bg-image('VHeader/invoice_1');
             }
-            &.special{
+            &.special {
               @include bg-image('VHeader/special_1');
             }
           }
@@ -137,7 +163,7 @@ export default {
         text-align: center;
         // display: flex;
         // align-items: center;
-        .count{
+        .count {
           display: inline-block;
           font-size: 10px;
           vertical-align: middle;
@@ -160,13 +186,13 @@ export default {
       overflow: hidden;
       text-overflow: ellipsis;
       .title {
-          display: inline-block;
-          width: 22px;
-          height: 12px;
-          @include bg-image('VHeader/bulletin');
-          background-size: 22px 12px;
-          background-repeat: no-repeat;
-          vertical-align: middle;
+        display: inline-block;
+        width: 22px;
+        height: 12px;
+        @include bg-image('VHeader/bulletin');
+        background-size: 22px 12px;
+        background-repeat: no-repeat;
+        vertical-align: middle;
       }
       .text {
         margin: 0 4px;
@@ -182,12 +208,44 @@ export default {
     }
     .background {
       position: absolute;
-      top: -74px;
+      top: -20%;
       right: 0;
       width: 100%;
       // height: 100px;
       z-index: -1;
       filter: blur(10px);
+    }
+    .detail {
+      top: 0;
+      right: 0;
+      position: fixed;
+      z-index: 1000;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      background: rgba(7, 17, 27, 0.8);
+      .detail-wrapper {
+        min-height: 100%;
+        width: 100%;
+        .detail-main {
+          margin-top: 64px;
+          padding-bottom: 64px;
+          .name {
+            line-height: 16px;
+            text-align: center;
+            font-size: 16px;
+            font-weight: 700;
+          }
+        }
+        .detail-close {
+          position: relative;
+          width: 32px;
+          height: 32px;
+          margin: -64px auto 0;
+          clear: both;
+          font-size: 32px;
+        }
+      }
     }
   }
 </style>
